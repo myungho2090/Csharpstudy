519f4f5bcf92a4831839d6c22a18ceac8174e486
myungho2090 <121885535+myungho2090@users.noreply.github.com>
2024-01-12 19:07:51 +0900

    c# 함수,객체
    1-12일 첫c#공부

---
+++ study/study/Program.cs
@@ -0,0 +1,85 @@
+﻿
+
+namespace study
+{
+    public class Program
+    {
+
+        static int Sum(int value1, int value2)
+        {
+
+            int result = value1 + value2;
+
+            return result;
+
+        }
+
+
+        public static void Main(string[] args)
+        {
+
+            fish fish1 = new fish()
+            {
+                source = "슈크림",
+                price = 500
+            };
+
+            fish fish2 = new fish() { 
+                source = "팥",
+                price = 300
+            };
+
+            int fish1_2 = fish1.price * 2;
+            int fish2_3 = fish2.price * 3;
+
+           int Sumresult = Sum(fish1_2, fish2_3);
+
+            //// See https://aka.ms/new-console-template for more information
+            Console.WriteLine("1번값을 입력해주세요.");
+
+
+            //string value1 = Console.ReadLine();
+
+            //bool isInt = Int32.TryParse(value1, out int intvalue1);
+
+            //if (!isInt) {
+
+
+
+            //    Console.WriteLine("숫자를 입력해주세요!");
+
+            //    return;
+
+            //}
+
+            ////int intvalue1 = Convert.ToInt32(value1);
+
+
+            //Console.WriteLine("2번값을 입력해주세요.");
+
+            //string value2 = Console.ReadLine();
+
+
+
+            ////string name = "정명호";
+
+            //Console.WriteLine(value1 + "값과" + value2 + "값을 더합니다.");
+
+            //int intvalue2 = Convert.ToInt32(value2);
+
+
+
+            //Console.WriteLine(intvalue1 + intvalue2);
+
+
+
+
+        }
+
+
+
+    }
+}
+
+
+
---
+++ study/study/fish.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study
+{
+    public class fish
+    {
+        public string source {  get; set; }
+
+        public int price { get; set; }
+
+    }
+}
